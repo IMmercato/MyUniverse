a94d88f668f73930e99ab64f1511f473156be125
IMmercato <imesh.fernando100@gmail.com>
2025-07-24 19:20:07 +0200

    Moving the cards into a deck.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import axios from "axios"
 
 function App() {
   const [array, setArray] = useState([])
+  const [selectedCardIndex, setSelectedCardIndex] = useState(null)
 
   const fetchAPI = async () => {
     const response = await axios.get("http://localhost:8080/api/yes")
@@ -16,33 +17,43 @@ function App() {
     fetchAPI()
   }, [])
 
+  const handleCard = (index) => {
+    setSelectedCardIndex(selectedCardIndex === index ? null : index)
+  }
+
   return (
     <>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
-    <div className='desktop'>
-      <div>
-        <i className='material-icons'>folder</i>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
+      <div className='desktop'>
+        <div>
+          <i className='material-icons'>folder</i>
+        </div>
       </div>
-    </div>
-      {
-        array.map((item, index) => (
-          <div key={index} className='card'>
-            <div className='nav'>
-              <div className='rarity'>{item.rarity}</div>
-              <h1>{item.name}</h1>
-              <div className='power'><h6>PS</h6><h1>{item.ps}</h1></div>
-              <i className="material-icons">sports_basketball</i>
-            </div>
-            <br />
-            <div className='player'>
-              <img src={lescream} alt="uaremysunshine" className='logo'/>
-            </div>
-            <div>
-              <p>{item.description}</p>
-            </div>
-          </div>
-        ))
-      }
+      <div className='deck'>
+        {
+          array.map((item, index) => {
+            const spread = 7
+            const angle = `${(index - array.length / 2) * spread}deg`;
+            return (
+              <div key={index} className={`card ${selectedCardIndex === index ? 'expanded' : 'minimized'}`} onClick={() => handleCard(index)} style={{ '--angle': angle }}>
+                <div className='nav'>
+                  <div className='rarity'>{item.rarity}</div>
+                  <h1>{item.name}</h1>
+                  <div className='power'><h6>PS</h6><h1>{item.ps}</h1></div>
+                  <i className="material-icons">sports_basketball</i>
+                </div>
+                <br />
+                <div className='player'>
+                  <img src={lescream} alt="uaremysunshine" className='logo' />
+                </div>
+                <div className='description'>
+                  <p>{item.description}</p>
+                </div>
+              </div>
+            );
+          })
+        }
+      </div >
     </>
   )
 }
