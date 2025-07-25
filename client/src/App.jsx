c39d4aa07847b1cd6b718c8711285ed4005c33aa
IMmercato <imesh.fernando100@gmail.com>
2025-07-25 12:07:02 +0200

    Using animejs for the windows

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,143 +1,106 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import { createDraggable } from 'animejs'
+import axios from 'axios'
 import './App.css'
-import lescream from "./assets/lebron-scream.svg"
-import lastnight from "./assets/lastnight.jpg"
-import leferrari from "./assets/leferrari.webp"
-import axios from "axios"
+import lescream from './assets/lebron-scream.svg'
+import lastnight from './assets/lastnight.jpg'
+import leferrari from './assets/leferrari.webp'
 
 function App() {
-  const [array, setArray] = useState([])
-  const [selectedCardIndex, setSelectedCardIndex] = useState(null)
+  const [cards, setCards] = useState([])
+  const [selected, setSelected] = useState(null)
   const [openWindow, setOpenWindow] = useState(false)
-  const [windowPosition, setWindowPosition] = useState({ x: 100, y: 100 })
-  const [isDragging, setIsDragging] = useState(false)
-  const [offset, setOffset] = useState({ x: 0, y: 0 })
-
-  const fetchAPI = async () => {
-    const response = await axios.get("http://localhost:8080/api/yes")
-    setArray(response.data);
-    console.log(response.data)
-  }
+  const winRef = useRef(null)
 
   useEffect(() => {
-    fetchAPI()
+    axios.get('http://localhost:8080/api/yes')
+      .then(res => setCards(res.data))
   }, [])
 
-  const handleCard = (index) => {
-    setSelectedCardIndex(selectedCardIndex === index ? null : index)
-  }
-
-  const toggleWindow = () => {
-    setOpenWindow(prevState => !prevState)
-  }
-  const closeWindow = () => {
-    setOpenWindow(false)
-  }
-
-  const handleMouseDown = (e) => {
-    setIsDragging(true)
-    setOffset({
-      x: e.clientX - windowPosition.x,
-      y: e.clientY - windowPosition.y
-    })
-  }
-  const handleMouseMove = (e) => {
-    if (isDragging) {
-      setWindowPosition({
-        x: e.clientX - offset.x,
-        y: e.clientY - offset.y
-      })
-    }
-  };
-  const handleMouseUp = () => {
-    setIsDragging(false)
-  }
-
   useEffect(() => {
-    if (isDragging) {
-      window.addEventListener('mousemove', handleMouseMove)
-      window.addEventListener('mouseup', handleMouseUp)
-    } else {
-      window.removeEventListener('mousemove', handleMouseMove)
-      window.removeEventListener('mouseup', handleMouseUp)
-    }
+    if (!openWindow || !winRef.current) return
+
+    const draggable = createDraggable(winRef.current, {
+      trigger: winRef.current.querySelector('.actions'),
+      container: document.body,
+      dragSpeed: 1
+    })
+
+    draggable.setX(100)
+    draggable.setY(100)
+
     return () => {
-      window.removeEventListener('mousemove', handleMouseMove)
-      window.removeEventListener('mouseup', handleMouseUp)
+      if (draggable && typeof draggable.destroy === 'function') {
+        draggable.destroy()
+      }
     }
-  }, [isDragging])
+  }, [openWindow]);
+
+  const handleCardClick = idx => setSelected(curr => (curr === idx ? null : idx))
+
+  const toggleWindow = () => setOpenWindow(v => !v)
+  const closeWindow = () => setOpenWindow(false)
 
   return (
     <>
-      <div className='desktop'>
-        <div className='folder' style={{ top: '20px', left: '50px' }} onClick={toggleWindow}>
-          <i className='material-icons'>folder</i>
+      <div className="desktop">
+        <div className="folder" style={{ top: 20, left: 50 }} onClick={toggleWindow}>
+          <i className="material-icons">folder</i>
           <p>Pics</p>
         </div>
-        <div className="folder" style={{ top: '80px', right: '20px' }}>
+        <div className="folder" style={{ top: 80, right: 20 }}>
           <i className="material-icons">folder</i>
           <p>Memory</p>
         </div>
       </div>
-      <div className='deck'>
-        {
-          array.map((item, index) => {
-            const spread = 7
-            const angle = `${(index - array.length / 2) * spread}deg`
-            return (
-              <div key={index} className={`card ${selectedCardIndex === index ? 'expanded' : 'minimized'}`} onClick={() => handleCard(index)} style={{ '--angle': angle }}>
-                <div className='nav'>
-                  <div className='rarity'>{item.rarity}</div>
-                  <h1>{item.name}</h1>
-                  <div className='power'><h6>PS</h6><h1>{item.ps}</h1></div>
-                  <i className="material-icons">sports_basketball</i>
+      <div className="deck">
+        {cards.map((card, i) => {
+          const spread = 7
+          const angle = `${(i - cards.length / 2) * spread}deg`
+          return (
+            <div
+              key={i}
+              className={`card ${selected === i ? 'expanded' : 'minimized'}`}
+              onClick={() => handleCardClick(i)}
+              style={{ '--angle': angle }}
+            >
+              <div className="nav">
+                <div className="rarity">{card.rarity}</div>
+                <h1>{card.name}</h1>
+                <div className="power">
+                  <h6>PS</h6><h1>{card.ps}</h1>
                 </div>
-                <br />
-                <div className='player'>
-                  <img src={lescream} alt="uaremysunshine" className='logo' />
-                </div>
-                <div className='description'>
-                  <p>{item.description}</p>
-                </div>
+                <i className="material-icons">sports_basketball</i>
               </div>
-            )
-          })
-        }
+              <div className="player">
+                <img src={lescream} alt="LeScream" className="logo" />
+              </div>
+              <div className="description">
+                <p>{card.description}</p>
+              </div>
+            </div>
+          )
+        })}
       </div>
       {openWindow && (
-        <div className='window' style={{ left: windowPosition.x, top: windowPosition.y }} onMouseDown={handleMouseDown}>
-          <div className='actions'>
-            <div>
-              <i className='material-icons'>minimize</i>
-            </div>
-            <div>
-              <i className='material-icons'>ad</i>
-            </div>
-            <div className='close' onClick={closeWindow}> {/* Close button */}
-              <i className='material-icons'>close</i>
+        <div ref={winRef} className="window">
+          <div className="actions">
+            <i className="material-icons">minimize</i>
+            <i className="material-icons">ad</i>
+            <div className="close" onClick={closeWindow}>
+              <i className="material-icons">close</i>
             </div>
           </div>
-          <div className='content'>
-            <div>
-              <img src={lastnight} alt="MyFriends" />
-              <p>Last Night</p>
-            </div>
-            <div>
-              <img src={leferrari} alt="Le-Ferrari" />
-              <p>'24</p>
-            </div>
-            <div>
-              <img src={lescream} alt="Le-Scream" />
-              <p>Le-Scream</p>
-            </div>
-            <div>
-              <img src={lastnight} alt="last" />
-              <p>Friends</p>
-            </div>
+          <div className="content">
+            <div><img src={lastnight} alt="" /><p>Last Night</p></div>
+            <div><img src={leferrari} alt="" /><p>'24</p></div>
+            <div><img src={lescream} alt="" /><p>Le-Scream</p></div>
+            <div><img src={lastnight} alt="" /><p>Friends</p></div>
           </div>
         </div>
       )}
     </>
   )
 }
+
 export default App
