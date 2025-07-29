7ddd2860465b893657ca4c395a7aa1bab816c75f
IMmercato <imesh.fernando100@gmail.com>
2025-07-29 13:56:04 +0200

    Update to run on a unique server

--- server/index.js
+++ server/index.js
@@ -1,24 +1,33 @@
 const express = require("express");
-const app = express();
+const path = require("path");
 const cors = require("cors");
-const corsOptions = {
-    origin: ["http://localhost:5173"],
-};
 const yes = require("./data/yes.json");
 const window = require("./data/window.json");
 
+const app = express();
+
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+};
 app.use(cors(corsOptions));
 
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
 app.get("/api", (req, res) => {
-    res.json({ names: ["lebronita", "Charse Lecrec", "Steph dududu Verstaphen"] });
+  res.json({ names: ["lebronita", "Charse Lecrec", "Steph dududu Verstaphen"] });
 });
 app.get("/api/yes", (req, res) => {
-    res.json(yes);
+  res.json(yes);
 });
 app.get("/api/window", (req, res) => {
-    res.json(window);
+  res.json(window);
 });
 
-app.listen(8080, () => {
-    console.log("Server started on port 8080");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
