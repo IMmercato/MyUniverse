61115298eb44709fe4154c65aadeec90e79e8bb4
IMmercato <imesh.fernando100@gmail.com>
2025-12-09 20:14:33 +0100

    Update

--- client/src/components/Browser.jsx
+++ client/src/components/Browser.jsx
@@ -10,7 +10,7 @@ const Browser = ({ onClose }) => {
     const iframeRef = useRef(null)
 
     const favoritesSites = [
-        { name: 'PH', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', icon: 'https://image2url.com/images/1765236533640-f05d6b6d-b3c5-4ca8-a44a-b399f65daca6.svg' },
+        { name: 'PH', url: 'https://www.youtube.com/embed/dQw4w9WgXcQ?si=fsUdFX2hHWwf5xlf&controls=0&autoplay=1', icon: 'https://image2url.com/images/1765236533640-f05d6b6d-b3c5-4ca8-a44a-b399f65daca6.svg' },
         { name: 'GutHib', url: 'https://github.com/', icon: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' },
         { name: 'localhost', url: 'http://localhost:3000', icon: 'https://cdn.iconscout.com/icon/free/png-512/free-language-icon-svg-download-png-1513808.png?f=webp&w=512' },
         { name: 'NBA', url: 'https://www.formula1.com/', icon: 'https://support.formula1.com/file-asset/F1_App_Red_Logo_White_Background?v=1' },
@@ -37,6 +37,33 @@ const Browser = ({ onClose }) => {
         handleNavigate(inputUrl)
     }
 
+    const handleBack = () => {
+        if (historyIndex > 0) {
+            const newIndex = historyIndex - 1
+            setHistoryIndex(newIndex)
+            setUrl(history[newIndex])
+            setInputUrl(history[newIndex])
+            setIsLoading(true)
+        }
+    }
+
+    const handleForward = () => {
+        if (historyIndex < history.length - 1) {
+            const newIndex = historyIndex + 1
+            setHistoryIndex(newIndex)
+            setUrl(history[newIndex])
+            setInputUrl(history[newIndex])
+            setIsLoading(true)
+        }
+    }
+
+    const handleRefresh = () => {
+        setIsLoading(true)
+        if (iframeRef.current) {
+            iframeRef.current.src = url
+        }
+    }
+
     const handleHome = () => {
         handleNavigate('https://www.google.com')
     }
@@ -51,9 +78,22 @@ const Browser = ({ onClose }) => {
                 <button className="browser-close" onClick={onClose}>
                     <i className="material-icons">close</i>
                 </button>
-                <button onClick={handleHome} title="Home">
-                    <i className="material-icons">home</i>
-                </button>
+            </div>
+            <div className="browser-toolbar">
+                <div className="navigation-buttons">
+                    <button onClick={handleBack} disabled={historyIndex === 0} title="Back">
+                        <i className="material-icons">arrow_back</i>
+                    </button>
+                    <button onClick={handleForward} disabled={historyIndex === history.length -1} title="Forward">
+                        <i className="material-icons">arrow_forward</i>
+                    </button>
+                    <button onClick={handleRefresh} title="Refresh">
+                        <i className="material-icons">refresh</i>
+                    </button>
+                    <button onClick={handleHome} title="Home">
+                        <i className="material-icons">home</i>
+                    </button>
+                </div>
 
                 <form onSubmit={handleSumbit} className="url-bar">
                     <i className="material-icons url-icon">
@@ -87,7 +127,7 @@ const Browser = ({ onClose }) => {
             </div>
 
             <div className="browser-content">
-                <iframe ref={iframeRef} src={url} title="Browser Content" className="browser-iframe" onLoad={() => setIsLoading(false)} sandbox="allow-same-origin allow-scripts allow-popups allow-forms" >
+                <iframe ref={iframeRef} src={url} title="Browser Content" className="browser-iframe" onLoad={() => setIsLoading(false)} sandbox="allow-same-origin allow-scripts allow-popups allow-forms allow-modals allow-top-navigation" referrerPolicy="no-referrer-when-downgrade" crossOrigin="anonymous" >
                     {isLoading && (
                         <div className="loading-overlay">
                             <div className="loading-spinner">
@@ -97,7 +137,18 @@ const Browser = ({ onClose }) => {
                     )}
                 </iframe>
             </div>
-        </div>
+
+            <div className="browser-footer">
+                <span className="status-text">
+                    {isLoading ? 'Loading...' : 'Ready'}
+                </span>
+                <span className="zoom-controls">
+                    <i className="material-icons">zoom_out</i>
+                    <span>100%</span>
+                    <i className="material-icons">zoom_in</i>
+                </span>
+            </div>
+        </div >
     )
 }
 
